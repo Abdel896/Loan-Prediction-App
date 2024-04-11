e93fe0f6756a6b4d12f006453ffc9b653a10b55c
Abdel896 <77315797+Abdel896@users.noreply.github.com>
2024-04-10 21:56:08 -0600

    Upload the fie my_app_v2.py

---
+++ my_app_v2.py
@@ -0,0 +1,140 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import base64
+
+# Function to get numerical value from dictionary
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):
+    feature_dict = {"No":1,"Yes":2}
+    for key,value in feature_dict.items():
+        if val == key:
+            return value
+
+# Function to get value from dictionary
+def get_value(val, my_dict):
+    for key, value in my_dict.items():
+        if val == key:
+            return value
+
+# Main title and image
+st.title('LOAN PREDICTION :')
+st.image('loan_image.jpg') 
+st.write('@DSU for learning purposes only') 
+
+# Selectbox to switch between pages
+app_mode = st.selectbox('Select Page', ['Home', 'Prediction'])
+
+# If Home page is selected
+if app_mode == 'Home':
+    pass
+
+# If Prediction page is selected
+elif app_mode == 'Prediction':
+    # Read CSV file
+    csv = pd.read_csv("train.csv")
+    #st.write(csv)
+
+    # Displaying image
+    #st.image('slider-short-3.jpg')
+
+    # Heading for input section
+    st.subheader('Sir/Mme, please fill all necessary information to get a reply to your loan request!')
+
+    # Creating columns for better layout
+    col1, col2 = st.columns(2)
+
+    # Inputs for client information
+    with col1:
+        st.subheader("Client Information:")
+        gender_dict = {"Male": 1, "Female": 2}
+        Gender = st.radio('Gender', tuple(gender_dict.keys()))
+
+        feature_dict = {"No": 1, "Yes": 2}
+        Married = st.radio('Married', tuple(feature_dict.keys()))
+        Self_Employed = st.radio('Self Employed', tuple(feature_dict.keys()))
+
+        Dependents = st.radio('Dependents', options=['0', '1', '2', '3+'])
+
+    with col2:
+        st.subheader("Financial Information:")
+        edu = {'Graduate': 1, 'Not Graduate': 2}
+        Education = st.radio('Education', tuple(edu.keys()))
+        ApplicantIncome = st.slider('Applicant Income', 0, 10000, 0)
+        CoapplicantIncome = st.slider('Coapplicant Income', 0, 10000, 0)
+        LoanAmount = st.slider('Loan Amount (in K$)', 9.0, 700.0, 200.0)
+        Loan_Amount_Term = st.selectbox('Loan Amount Term', (12.0, 36.0, 60.0, 84.0, 120.0, 180.0, 240.0, 300.0, 360.0))
+        Credit_History = st.radio('Credit History', (0.0, 1.0))
+
+        prop = {'Rural': 1, 'Urban': 2, 'Semiurban': 3}
+        Property_Area = st.radio('Property Area', tuple(prop.keys()))
+
+    # Converting inputs into numerical format
+    class_0, class_3, class_1, class_2 = 0, 0, 0, 0
+    if Dependents == '0':
+        class_0 = 1
+    elif Dependents == '1':
+        class_1 = 1
+    elif Dependents == '2':
+        class_2 = 1
+    else:
+        class_3 = 1
+
+    Rural, Urban, Semiurban = 0, 0, 0
+    if Property_Area == 'Urban':
+        Urban = 1
+    elif Property_Area == 'Semiurban':
+        Semiurban = 1
+    else:
+        Rural = 1
+
+    # Storing input values
+    data1 = {
+        'Gender': Gender,
+        'Married': Married,
+        'Dependents': [class_0, class_1, class_2, class_3],
+        'Education': Education,
+        'ApplicantIncome': ApplicantIncome,
+        'CoapplicantIncome': CoapplicantIncome,
+        'Self Employed': Self_Employed,
+        'LoanAmount': LoanAmount,
+        'Loan_Amount_Term': Loan_Amount_Term,
+        'Credit_History': Credit_History,
+        'Property_Area': [Rural, Urban, Semiurban],
+    }
+
+    feature_list = [
+        ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History,
+        get_value(Gender, gender_dict), get_fvalue(Married), data1['Dependents'][0], data1['Dependents'][1],
+        data1['Dependents'][2], data1['Dependents'][3], get_value(Education, edu), get_fvalue(Self_Employed),
+        data1['Property_Area'][0], data1['Property_Area'][1], data1['Property_Area'][2]
+    ]
+
+    single_sample = np.array(feature_list).reshape(1, -1)
+
+    # Prediction button
+    if st.button("Predict"):
+        file_ = open("6m-rain.gif", "rb")
+        contents = file_.read()
+        data_url = base64.b64encode(contents).decode("utf-8")
+        file_.close()
+
+        file = open("green-cola-no.gif", "rb")
+        contents = file.read()
+        data_url_no = base64.b64encode(contents).decode("utf-8")
+        file.close()
+
+        # Load model
+        loaded_model = pickle.load(open('RF.sav', 'rb'))
+
+        # Make prediction
+        prediction = loaded_model.predict(single_sample)
+
+        # Display prediction result
+        if prediction[0] == 0:
+            st.error('According to our calculations, you will not get the loan from the bank.')
+            st.markdown(f'<img src="data:image/gif;base64,{data_url_no}" alt="cat gif">', unsafe_allow_html=True)
+        elif prediction[0] == 1:
+            st.success('Congratulations! You will get the loan from the bank.')
+            st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">', unsafe_allow_html=True)
